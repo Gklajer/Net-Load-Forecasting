43025815e594bc560524e96c87524daf3fd2be7b
GKLAJER <gary.klajer@outlook.com>
2024-03-03 18:05:47 +0100

    Good submission

--- Utils/utils.R
+++ Utils/utils.R
@@ -16,7 +16,7 @@ train_eval <- function(mod_cls, eq, train_data, test_data, is.rq=F, ...) {
 
 is.wholenumber <- function(x, tol = .Machine$double.eps^0.5) abs(x - round(x)) < tol
 
-time_cv <- function(mod_cls, eq, data, trainset_size, testset_size=1, nb_eval=1, step=0, type="rolling", is.rq=F, quant_fun=quantile, quant_p=.95, ...) {
+time_cv <- function(mod_cls, eq, data, trainset_size, testset_size=1, nb_eval=0, step=0, type="rolling", is.rq=F, quant_fun=quantile, quant_p=.95, pb_p=.95, ...) {
   trainval_size = trainset_size + testset_size
   excess_data_size = nrow(data) - trainval_size
   
@@ -25,10 +25,16 @@ time_cv <- function(mod_cls, eq, data, trainset_size, testset_size=1, nb_eval=1,
   try(if(!(all(is.wholenumber(c(trainset_size, testset_size, nb_eval, step))))) 
     stop("The arguments trainset_size, testset_size, nb_eval and step must be whole numbers!"))
   
+  if(nb_eval == 0 & step == 0) {
+    step = testset_size
+    nb_eval = floor(excess_data_size / step) + 1
+  }
+  
   try(if(nb_eval < 1 | excess_data_size < (nb_eval - 1)) 
     stop("Wrong combination of trainset_size, testset_size and nb_eval!"))
   
-  maxstep = ifelse(nb_eval == 1, 1, round(excess_data_size / (nb_eval - 1)))
+  maxstep = ifelse(nb_eval == 1, Inf, floor(excess_data_size / (nb_eval - 1)))
+
   step = ifelse(step == 0, maxstep, step)
   try(if(step < 1 | step > maxstep) 
     stop("Argument step provided is not valid!"))
@@ -40,9 +46,9 @@ time_cv <- function(mod_cls, eq, data, trainset_size, testset_size=1, nb_eval=1,
   if(!is.rq) cv$res = list(val = cv$pred)
   
   for(i in 1:nb_eval) {
-    cv$idx$start[i] = switch(type, "rolling"=1, "window"=1 + (i-1) * step)
-    cv$idx$mid[i] = trainset_size + (i-1)*step
-    cv$idx$end[i] = cv$idx$mid[i] + testset_size
+    cv$idx$start[i] = switch(type, "rolling"=1, "window"=1 + (i-1)*step)
+    cv$idx$mid[i] = (1 + (i-1)*step) + trainset_size - 1 
+    cv$idx$end[i] = (cv$idx$mid[i] + 1) + testset_size - 1
     
     test_data = data[(cv$idx$mid[i]+1):cv$idx$end[i],]
     
@@ -54,7 +60,7 @@ time_cv <- function(mod_cls, eq, data, trainset_size, testset_size=1, nb_eval=1,
     
     if(is.rq) {
       cv$res$pb[i] <- pinball_loss(test_data[,as.character(eq[[2]])], 
-                                   result$pred, quant_p,
+                                   result$pred, pb_p,
                                    output.vect = T)
       next
     }
@@ -70,7 +76,7 @@ time_cv <- function(mod_cls, eq, data, trainset_size, testset_size=1, nb_eval=1,
                               quant_fun(quant_p, mean(result$res), sd(result$res)))
     
     cv$res$pb[i] <-  pinball_loss(test_data[,as.character(eq[[2]])], 
-                                  result$pred + cv$res$quant[i], quant_p, 
+                                  result$pred + cv$res$quant[i], pb_p, 
                                   output.vect = T)
   }
   
@@ -78,6 +84,10 @@ time_cv <- function(mod_cls, eq, data, trainset_size, testset_size=1, nb_eval=1,
 }
 
 which.median <- function(x) which.min(x[x>=median(x)])
+
+sliding_quantile <- function(x, window_size, quantile_value) {
+  slide_dbl(x, .f = ~ quantile(.x, quantile_value), .before = window_size-1, .complete = TRUE)
+}
 
 create_submission <- function(mod_cls, eq, train_data = Data0, pred_data = Data1, quant=0, ...) {
   mod = mod_cls(as.formula(eq), data=train_data, ...)
